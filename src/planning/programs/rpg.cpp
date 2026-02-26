6d98925533cc2aa8afc8c9bec54cd854c06bd2d7
Dominik Drexler <drexlerd93@gmail.com>
2026-02-26 23:59:06 +0100

    rpg with app

--- src/planning/programs/rpg.cpp
+++ src/planning/programs/rpg.cpp
@@ -36,10 +36,264 @@ namespace tyr::planning
 {
 namespace rpg
 {
+template<f::FactKind T>
+void collect_parameters(View<Index<fp::Atom<T>>, fp::Repository> atom, UnorderedSet<f::ParameterIndex>& result)
+{
+    for (const auto term : atom.get_terms())
+    {
+        visit(
+            [&](auto&& arg)
+            {
+                using Alternative = std::decay_t<decltype(arg)>;
+
+                if constexpr (std::is_same_v<Alternative, f::ParameterIndex>)
+                    result.insert(arg);
+                else if constexpr (std::is_same_v<Alternative, View<Index<f::Object>, fp::Repository>>) {}
+                else
+                    static_assert(dependent_false<Alternative>::value, "Missing case");
+            },
+            term.get_variant());
+    }
+}
+
+template<f::FactKind T>
+void collect_parameters(View<Index<fp::Literal<T>>, fp::Repository> literal, UnorderedSet<f::ParameterIndex>& result)
+{
+    collect_parameters(literal.get_atom(), result);
+}
+
+static void collect_parameters(View<Index<fp::ConjunctiveCondition>, fp::Repository> condition, UnorderedSet<f::ParameterIndex>& result)
+{
+    for (const auto literal : condition.get_literals<f::StaticTag>())
+        collect_parameters(literal, result);
+
+    for (const auto literal : condition.get_literals<f::FluentTag>())
+        if (literal.get_polarity())
+            collect_parameters(literal, result);
+}
+
+static void collect_parameters(View<Index<fp::ConjunctiveEffect>, fp::Repository> effect, UnorderedSet<f::ParameterIndex>& result)
+{
+    for (const auto literal : effect.get_literals())
+        if (literal.get_polarity())
+            collect_parameters(literal, result);
+}
+
+static void collect_parameters(View<Index<fp::ConditionalEffect>, fp::Repository> effect, UnorderedSet<f::ParameterIndex>& result)
+{
+    collect_parameters(effect.get_condition(), result);
+    collect_parameters(effect.get_effect(), result);
+}
+
+static void collect_parameters(View<Index<fp::Action>, fp::Repository> action, UnorderedSet<f::ParameterIndex>& result)
+{
+    for (const auto effect : action.get_effects())
+        collect_parameters(effect, result);
+}
+
+static auto sorted_params(const UnorderedSet<f::ParameterIndex>& s)
+{
+    std::vector<f::ParameterIndex> v(s.begin(), s.end());
+    std::sort(v.begin(), v.end(), [](auto a, auto b) { return uint_t(a) < uint_t(b); });
+    return v;
+}
+
+static bool should_keep(View<Data<f::Term>, fp::Repository> term, const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping)
+{
+    return visit(
+        [&](auto&& arg)
+        {
+            using Alternative = std::decay_t<decltype(arg)>;
+
+            if constexpr (std::is_same_v<Alternative, f::ParameterIndex>)
+                return mapping.contains(arg);
+            else if constexpr (std::is_same_v<Alternative, View<Index<f::Object>, fp::Repository>>)
+                return true;
+            else
+                static_assert(dependent_false<Alternative>::value, "Missing case");
+        },
+        term.get_variant());
+}
+
+template<f::FactKind T>
+bool should_keep(View<Index<fp::Atom<T>>, fp::Repository> atom, const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping)
+{
+    return std::all_of(atom.get_terms().begin(), atom.get_terms().end(), [&](auto&& term) { return should_keep(term, mapping); });
+}
+
+static bool should_keep(View<Index<fp::Literal<f::StaticTag>>, fp::Repository> literal, const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping)
+{
+    return should_keep(literal.get_atom(), mapping);
+}
+
+static bool should_keep(View<Index<fp::Literal<f::FluentTag>>, fp::Repository> literal, const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping)
+{
+    return literal.get_polarity() && should_keep(literal.get_atom(), mapping);
+}
+
+static bool should_keep(View<Index<fp::Literal<f::StaticTag>>, fp::Repository> literal) { return true; }
+
+static bool should_keep(View<Index<fp::Literal<f::FluentTag>>, fp::Repository> literal) { return literal.get_polarity(); }
+
+static auto merge(View<Data<f::Term>, fp::Repository> element, const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping)
+{
+    return visit(
+        [&](auto&& arg)
+        {
+            using Alternative = std::decay_t<decltype(arg)>;
+
+            if constexpr (std::is_same_v<Alternative, f::ParameterIndex>)
+                return Data<f::Term>(mapping.at(arg));
+            else if constexpr (std::is_same_v<Alternative, View<Index<f::Object>, fp::Repository>>)
+                return Data<f::Term>(arg.get_index());
+            else
+                static_assert(dependent_false<Alternative>::value, "Missing case");
+        },
+        element.get_variant());
+}
+
+template<f::FactKind T>
+auto merge(View<Index<fp::Atom<T>>, fp::Repository> element,
+           const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping,
+           formalism::planning::MergeDatalogContext& context)
+{
+    auto atom_ptr = context.builder.get_builder<fd::Atom<T>>();
+    auto& atom = *atom_ptr;
+    atom.clear();
+
+    atom.predicate = element.get_predicate().get_index();
+    for (const auto term : element.get_terms())
+        atom.terms.push_back(merge(term, mapping));
+
+    canonicalize(atom);
+    return context.destination.get_or_create(atom, context.builder.get_buffer());
+}
+
+template<f::FactKind T>
+auto merge(View<Index<fp::Literal<T>>, fp::Repository> element,
+           const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping,
+           formalism::planning::MergeDatalogContext& context)
+{
+    auto literal_ptr = context.builder.get_builder<fd::Literal<T>>();
+    auto& literal = *literal_ptr;
+    literal.clear();
+
+    literal.polarity = element.get_polarity();
+    literal.atom = merge(element.get_atom(), mapping, context).first;
+
+    canonicalize(literal);
+    return context.destination.get_or_create(literal, context.builder.get_buffer());
+}
+
+static auto create_applicability_predicate(View<Index<formalism::planning::Action>, formalism::planning::Repository> action,
+                                           const UnorderedSet<f::ParameterIndex>& parameters,
+                                           formalism::planning::MergeDatalogContext& context)
+{
+    auto predicate_ptr = context.builder.get_builder<formalism::Predicate<formalism::FluentTag>>();
+    auto& predicate = *predicate_ptr;
+    predicate.clear();
+
+    predicate.name = create_applicability_name(action);
+    predicate.arity = parameters.size();
+
+    canonicalize(predicate);
+    return context.destination.get_or_create(predicate, context.builder.get_buffer());
+}
+
+static auto create_applicability_atom(View<Index<formalism::planning::Action>, formalism::planning::Repository> action,
+                                      const UnorderedSet<f::ParameterIndex>& parameters,
+                                      formalism::planning::MergeDatalogContext& context)
+{
+    auto atom_ptr = context.builder.get_builder<formalism::datalog::Atom<formalism::FluentTag>>();
+    auto& atom = *atom_ptr;
+    atom.clear();
+
+    const auto applicability_predicate = make_view(create_applicability_predicate(action, parameters, context).first, context.destination);
+
+    atom.predicate = applicability_predicate.get_index();
+    for (const auto param : parameters)
+        atom.terms.push_back(Data<formalism::Term>(param));
+
+    canonicalize(atom);
+    return context.destination.get_or_create(atom, context.builder.get_buffer());
+}
+
+static auto create_applicability_rule(View<Index<formalism::planning::Action>, formalism::planning::Repository> action,
+                                      const UnorderedSet<f::ParameterIndex>& parameters,
+                                      formalism::planning::MergeDatalogContext& context)
+{
+    auto rule_ptr = context.builder.get_builder<formalism::datalog::Rule>();
+    auto& rule = *rule_ptr;
+    rule.clear();
+
+    auto conj_cond_ptr = context.builder.get_builder<formalism::datalog::ConjunctiveCondition>();
+    auto& conj_cond = *conj_cond_ptr;
+    conj_cond.clear();
+
+    for (const auto variable : action.get_variables())
+        conj_cond.variables.push_back(merge_p2d(variable, context).first);
+
+    // Keep negated static atoms because they are monotonic
+    for (const auto literal : action.get_condition().template get_literals<formalism::StaticTag>())
+        if (should_keep(literal))
+            conj_cond.static_literals.push_back(merge_p2d(literal, context).first);
+
+    for (const auto literal : action.get_condition().template get_literals<formalism::FluentTag>())
+        if (should_keep(literal))
+            conj_cond.fluent_literals.push_back(merge_p2d(literal, context).first);
+
+    canonicalize(conj_cond);
+    const auto new_conj_cond = make_view(context.destination.get_or_create(conj_cond, context.builder.get_buffer()).first, context.destination);
+
+    rule.variables = new_conj_cond.get_variables().get_data();
+    rule.body = new_conj_cond.get_index();
+    rule.head = create_applicability_atom(action, parameters, context).first;
+    rule.cost = uint_t(0);
+
+    canonicalize(rule);
+    return context.destination.get_or_create(rule, context.builder.get_buffer());
+}
+
+static auto create_applicability_atom(View<Index<formalism::planning::Action>, formalism::planning::Repository> action,
+                                      const UnorderedSet<f::ParameterIndex>& head_parameters,
+                                      const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping,
+                                      formalism::planning::MergeDatalogContext& context)
+{
+    auto atom_ptr = context.builder.get_builder<formalism::datalog::Atom<formalism::FluentTag>>();
+    auto& atom = *atom_ptr;
+    atom.clear();
+
+    const auto applicability_predicate = make_view(create_applicability_predicate(action, head_parameters, context).first, context.destination);
+
+    atom.predicate = applicability_predicate.get_index();
+    for (const auto param : head_parameters)
+        atom.terms.push_back(Data<formalism::Term>(mapping.at(param)));
+
+    canonicalize(atom);
+    return context.destination.get_or_create(atom, context.builder.get_buffer());
+}
+
+static auto create_applicability_literal(View<Index<formalism::planning::Action>, formalism::planning::Repository> action,
+                                         const UnorderedSet<f::ParameterIndex>& head_parameters,
+                                         const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping,
+                                         formalism::planning::MergeDatalogContext& context)
+{
+    auto literal_ptr = context.builder.get_builder<formalism::datalog::Literal<formalism::FluentTag>>();
+    auto& literal = *literal_ptr;
+    literal.clear();
+
+    literal.polarity = true;
+    literal.atom = create_applicability_atom(action, head_parameters, mapping, context).first;
+
+    canonicalize(literal);
+    return context.destination.get_or_create(literal, context.builder.get_buffer());
+}
 
 static auto create_cond_effect_rule(View<Index<formalism::planning::Action>, formalism::planning::Repository> action,
                                     View<Index<formalism::planning::ConditionalEffect>, formalism::planning::Repository> cond_eff,
                                     View<Index<formalism::planning::Atom<formalism::FluentTag>>, formalism::planning::Repository> effect,
+                                    const UnorderedSet<f::ParameterIndex>& head_parameters,
+                                    const UnorderedMap<f::ParameterIndex, f::ParameterIndex>& mapping,
                                     formalism::planning::MergeDatalogContext& context)
 {
     auto rule_ptr = context.builder.get_builder<formalism::datalog::Rule>();
@@ -51,32 +305,85 @@ static auto create_cond_effect_rule(View<Index<formalism::planning::Action>, for
     conj_cond.clear();
 
     // Action parameter may get deleted.
-    for (const auto& variable : action.get_variables())
-        conj_cond.variables.push_back(merge_p2d(variable, context).first);
+    for (uint_t param = 0; param < action.get_arity(); ++param)
+        if (mapping.contains(f::ParameterIndex(param)))
+            conj_cond.variables.push_back(merge_p2d(action.get_variables()[uint_t(param)], context).first);
     for (const auto literal : action.get_condition().get_literals<formalism::StaticTag>())
-        conj_cond.static_literals.push_back(merge_p2d(literal, context).first);
-    for (const auto literal : action.get_condition().get_literals<formalism::FluentTag>())
-        if (literal.get_polarity())
-            conj_cond.fluent_literals.push_back(merge_p2d(literal, context).first);
+        if (should_keep(literal, mapping))
+            conj_cond.static_literals.push_back(merge(literal, mapping, context).first);
+    conj_cond.fluent_literals.push_back(create_applicability_literal(action, head_parameters, mapping, context).first);
 
     for (const auto variable : cond_eff.get_variables())
         conj_cond.variables.push_back(merge_p2d(variable, context).first);
+
     for (const auto literal : cond_eff.get_condition().template get_literals<formalism::StaticTag>())
-        conj_cond.static_literals.push_back(merge_p2d(literal, context).first);
+        if (should_keep(literal, mapping))
+            conj_cond.static_literals.push_back(merge(literal, mapping, context).first);
+
     for (const auto literal : cond_eff.get_condition().template get_literals<formalism::FluentTag>())
-        if (literal.get_polarity())
-            conj_cond.fluent_literals.push_back(merge_p2d(literal, context).first);
+        if (should_keep(literal, mapping))
+            conj_cond.fluent_literals.push_back(merge(literal, mapping, context).first);
 
     canonicalize(conj_cond);
     const auto new_conj_cond = make_view(context.destination.get_or_create(conj_cond, context.builder.get_buffer()).first, context.destination);
 
     rule.variables = new_conj_cond.get_variables().get_data();
     rule.body = new_conj_cond.get_index();
-    rule.head = merge_p2d(effect, context).first;
-    rule.cost = 1;
+    rule.head = merge(effect, mapping, context).first;
+    rule.cost = uint_t(1);
 
     canonicalize(rule);
     return context.destination.get_or_create(rule, context.builder.get_buffer());
+}
+
+auto compute_applicability_head_parameters(View<Index<fp::Action>, fp::Repository> action)
+{
+    // Collect all parameter indices mentioned in positive fluent literals or negative static literals
+    auto parameters = UnorderedSet<f::ParameterIndex> {};
+    collect_parameters(action, parameters);
+
+    // Remove parameters that are bound in any conditional effect effect
+    for (auto it = parameters.begin(); it != parameters.end();)
+    {
+        if (uint_t(*it) >= action.get_arity())
+            it = parameters.erase(it);
+        else
+            ++it;
+    }
+
+    return parameters;
+}
+
+auto compute_applicability_parameter_mapping(View<Index<fp::Action>, fp::Repository> action,
+                                             View<Index<fp::ConditionalEffect>, fp::Repository> effect,
+                                             const UnorderedSet<f::ParameterIndex>& head_parameters)
+{
+    auto mapping = UnorderedMap<f::ParameterIndex, f::ParameterIndex> {};
+
+    auto new_param = uint_t(0);
+    // Sort by original parameter index so the remapping is order-preserving:
+    // kept parameters are condensed to 0..k-1 while preserving their relative order.
+    for (const auto& param : sorted_params(head_parameters))
+        mapping.emplace(param, f::ParameterIndex { new_param++ });
+
+    // Collect parameters that are bound in the conditional effect.
+    auto effect_parameters = UnorderedSet<f::ParameterIndex> {};
+    collect_parameters(effect, effect_parameters);
+
+    // Remove parameter that are bound in the action.
+    for (auto it = effect_parameters.begin(); it != effect_parameters.end();)
+    {
+        if (uint_t(*it) < action.get_arity())
+            it = effect_parameters.erase(it);
+        else
+            ++it;
+    }
+
+    // Sort for same reason as above.
+    for (const auto& param : sorted_params(effect_parameters))
+        mapping.emplace(param, f::ParameterIndex { new_param++ });
+
+    return mapping;
 }
 
 static void translate_action_to_delete_free_rules(View<Index<fp::Action>, fp::Repository> action,
@@ -84,17 +391,30 @@ static void translate_action_to_delete_free_rules(View<Index<fp::Action>, fp::Re
                                                   fp::MergeDatalogContext& context,
                                                   RPGProgram::RuleToActionMapping& rule_to_action)
 {
+    // Collect all parameter indices mentioned in positive fluent literals or negative static literals
+    auto head_parameters = compute_applicability_head_parameters(action);
+
+    const auto applicability_predicate = create_applicability_predicate(action, head_parameters, context).first;
+
+    program.fluent_predicates.push_back(applicability_predicate);
+
+    const auto applicability_rule = create_applicability_rule(action, head_parameters, context).first;
+
+    [[maybe_unused]] const auto [it, inserted] = rule_to_action.emplace(applicability_rule, action.get_index());
+    assert(inserted);
+
+    program.rules.push_back(applicability_rule);
+
     for (const auto cond_eff : action.get_effects())
     {
+        auto parameter_mapping = compute_applicability_parameter_mapping(action, cond_eff, head_parameters);
+
         for (const auto literal : cond_eff.get_effect().get_literals())
         {
             if (!literal.get_polarity())
                 continue;  /// ignore delete effects
 
-            const auto rule = create_cond_effect_rule(action, cond_eff, literal.get_atom(), context).first;
-
-            program.rules.push_back(rule);
-            rule_to_action.emplace(rule, action.get_index());
+            program.rules.push_back(create_cond_effect_rule(action, cond_eff, literal.get_atom(), head_parameters, parameter_mapping, context).first);
         }
     }
 }
